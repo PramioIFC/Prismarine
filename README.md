9ef6f926de1300d8f1e4955a2547e8c93587eee4
garfo <gaffixlol@gmail.com>
2026-02-09 19:23:45 -0300

    base

---
+++ README.md
@@ -0,0 +1,10 @@
+# Prismarine
+## frase insana que nos representa
+
+## Como usar
+
+```bash
+npm install
+```
+
+- **`npm run dev`** — Start dev server with HMR
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="app">
+      <h1>Prismarine</h1>
+      <p>*isso aqui é o <code>src/App.jsx</code></p>
+    </div>
+  )
+}
+
+export default App
