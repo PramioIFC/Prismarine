026b8d6083609e86cb2b1c26ae9da4cc4f45a76e
Bruno Gelain <gaffixlol@gmail.com>
2026-02-25 07:58:51 -0300

    seções

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,35 @@ function App() {
         <span className="header-logo">Prismarine</span>
         <nav className="header-nav">
           <a href="#inicio">Início</a>
+          <a href="#objetivos">Objetivos</a>
+          <a href="#servicos">Serviços</a>
+          <a href="#equipe">Equipe</a>
           <a href="#sobre">Sobre Nós</a>
-          <a href="#contato">Contato? nao sei</a>
         </nav>
       </header>
 
       <main className="app">
-        <img id="prismarineLogo" src={prismarineLogo} alt="Prismarine" />
-        <h1 id="prismarineTitle">Prismarine</h1>
-        <p>*isso aqui é o <code>src/App.jsx</code></p>
+        <div id="inicio" className="section hero">
+          <img id="prismarineLogo" src={prismarineLogo} alt="Prismarine" />
+          <h1 id="prismarineTitle">Prismarine</h1>
+          <h2>Início</h2>
+        </div>
+
+        <div id="objetivos" className="section objetivos">
+          <h2>Objetivos</h2>
+        </div>
+
+        <div id="servicos" className="section servicos">
+          <h2>Serviços</h2>
+        </div>
+
+        <div id="equipe" className="section equipe">
+          <h2>Equipe</h2>
+        </div>
+
+        <div id="sobre" className="section sobre">
+          <h2>Sobre Nós</h2>
+        </div>
       </main>
 
       <footer className="footer">
