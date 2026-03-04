f92a874d7a50afbe78f4afff0d96c0054fa15471
João Vitor Pramio <joaovitorp7777@gmail.com>
2026-03-04 09:25:01 -0300

    sessões novas

--- src/App.jsx
+++ src/App.jsx
@@ -57,8 +57,10 @@ function App() {
           <a href="#inicio" onClick={() => setMobileNavOpen(false)}>Início</a>
           <a href="#objetivos" onClick={() => setMobileNavOpen(false)}>Objetivos</a>
           <a href="#servicos" onClick={() => setMobileNavOpen(false)}>Serviços</a>
+          <a href="#flux" onClick={() => setMobileNavOpen(false)}>Flux</a>
           <a href="#equipe" onClick={() => setMobileNavOpen(false)}>Equipe</a>
           <a href="#sobre" onClick={() => setMobileNavOpen(false)}>Sobre Nós</a>
+          <a href="#contato" onClick={() => setMobileNavOpen(false)}>Contato</a>
         </nav>
       </header>
 
